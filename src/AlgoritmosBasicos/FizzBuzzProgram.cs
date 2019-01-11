c3b3061edfb593d23e5a79a61a48e39604c3c407
Benjamin Camacho <benjamin.camacho@dabarc.com>
2019-01-11 17:03:09 -0600

    Se agrega FizzBuzz

--- src/AlgoritmosBasicos/FizzBuzzProgram.cs
+++ src/AlgoritmosBasicos/FizzBuzzProgram.cs
@@ -13,13 +13,8 @@
         /// <returns>decimal</returns>
         public string FizzBuzz(int number)
         {
-            if (number % 3 == 0 && number % 5 == 0)
-                return "FizzBuzz";
-            if (number % 3 == 0)
-                return "Fizz";
-            if (number % 5 == 0)
-                return "Buzz";
-            return number.ToString();
+            
+            return string.Empty;
         }
     }
 }
