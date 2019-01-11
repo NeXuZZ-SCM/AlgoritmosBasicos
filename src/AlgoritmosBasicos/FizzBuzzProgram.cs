2f36fc0f530d90f00e66e701b458de15fbfac058
Benjamin Camacho <benjamin.camacho@dabarc.com>
2019-01-11 17:02:37 -0600

    Se agrega FizzBuzz

---
+++ src/AlgoritmosBasicos/FizzBuzzProgram.cs
@@ -0,0 +1,25 @@
+﻿namespace AlgoritmosBasicos
+{
+    public class FizzBuzzProgram
+    {
+        /// <summary>
+        /// Recibe un número entrero positivo  y regresa:
+        /// la palabra Fizz si el número es multiplo de 3
+        /// la palabra Buzz si el número es multiplo de 5
+        /// la palabra FizzBuzz si el número es multiplo de 3 y 5
+        /// el numero  si no es multiplo de 3 y 5
+        /// </summary>
+        /// <param name="celsius">Indica el valor en grados celcius</param>
+        /// <returns>decimal</returns>
+        public string FizzBuzz(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+            if (number % 3 == 0)
+                return "Fizz";
+            if (number % 5 == 0)
+                return "Buzz";
+            return number.ToString();
+        }
+    }
+}
---
+++ test/AlgoritmosBasicos.Tests/FizzBuzzProgramTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace AlgoritmosBasicos.Tests
+{
+    public class FizzBuzzProgramTest
+    {
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(3, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(97, "97")]
+        [InlineData(100, "Buzz")]
+        [InlineData(15, "FizzBuzz")]
+
+        public void FizzBuzzTest(int numerp, string valorEsperado)
+        {
+            var fizzBuzz = new FizzBuzzProgram();
+            var resultado = fizzBuzz.FizzBuzz(numerp);
+            Assert.Equal(valorEsperado, resultado);
+        }
+    }
+}
